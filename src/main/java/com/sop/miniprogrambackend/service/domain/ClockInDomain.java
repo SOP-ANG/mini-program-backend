871757afc9c7bc198238015274184281ea15ee68
SOP-ANG <muzhdmhxy@163.com>
2019-06-24 20:57:27 +0800

    [IMP]后台界面

---
+++ src/main/java/com/sop/miniprogrambackend/service/domain/ClockInDomain.java
@@ -0,0 +1,12 @@
+package com.sop.miniprogrambackend.service.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ClockInDomain {
+    private Integer id;
+}
