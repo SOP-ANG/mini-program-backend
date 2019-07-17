dd4a61a201c1127b888df4914102f72f45d2bb78
SOP-ANG <muzhdmhxy@163.com>
2019-07-18 00:39:47 +0800

    [IMP]增加登录

--- src/main/java/com/sop/miniprogrambackend/controller/BackendController.java
+++ src/main/java/com/sop/miniprogrambackend/controller/BackendController.java
@@ -1,6 +1,8 @@
 package com.sop.miniprogrambackend.controller;
 
 import com.sop.miniprogrambackend.functional.response.ResponseException;
+import com.sop.miniprogrambackend.model.AdminDOMapper;
+import com.sop.miniprogrambackend.model.data.AdminDO;
 import com.sop.miniprogrambackend.service.BackendService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -20,17 +23,26 @@ public class BackendController extends BaseController {
     @Autowired
     private BackendService backendService;
 
+    //这个通过 Spring bean 包装的 httpservletrequest 其本质是一个 proxy，它内部拥有 thread local 的 map，让用户在每一个线程中处理它自己对应的 request，并且有 thread local 清除的机制，因此可以支持多个用户并发的访问
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
     /**
      * 首页
      * @param paramMap
      * @return
      */
-    @RequestMapping("/")
-    public String index(Map<String, Object> paramMap) {
+    @RequestMapping("/dashboard")
+    public String dashboard(Map<String, Object> paramMap) {
+        Boolean isLogin = (Boolean) httpServletRequest.getSession().getAttribute("IS_LOGIN");
+        if(isLogin == null || !isLogin.booleanValue()) {
+            return "loginfailed";
+        }
+
         List<Integer> memberIds = this.backendService.getMemberIds();
         paramMap.put("member_count", memberIds.size());
         paramMap.put("finished_member_count", this.backendService.getFinishedMemberCount(memberIds));
-        return "index";
+        return "dashboard";
     }
 
     /**
@@ -40,6 +52,11 @@ public class BackendController extends BaseController {
      */
     @RequestMapping("/userlist")
     public String getUserList(Map<String, Object> paramMap) {
+        Boolean isLogin = (Boolean) httpServletRequest.getSession().getAttribute("IS_LOGIN");
+        if(isLogin == null || !isLogin.booleanValue()) {
+            return "loginfailed";
+        }
+
         paramMap.put("userlist", this.backendService.getUsersInfo().stream().map(
                 this::convertUserFromDomain).collect(Collectors.toList()));
         return "table";
@@ -56,11 +73,51 @@ public class BackendController extends BaseController {
     public String getDetail(Map<String, Object> paramMap,
                             @RequestParam(name = "userId") Integer userId,
                             @RequestParam(name = "nickName") String nickName) throws ResponseException {
+        Boolean isLogin = (Boolean) httpServletRequest.getSession().getAttribute("IS_LOGIN");
+        if(isLogin == null || !isLogin.booleanValue()) {
+            return "loginfailed";
+        }
+
         paramMap.put("userId", userId);
         paramMap.put("nickName", nickName);
 
         paramMap.put("clockInlist", this.backendService.getFinishedClockInList(userId).stream().map(
                 this::convertClockInFromDomain).collect(Collectors.toList()));
         return "detail";
+    }
+
+    ////////////////////////////////////// 临时功能 ///////////////////////////////////////////
+
+    /**
+     * 登录页（临时工能）
+     * @return
+     */
+    @RequestMapping("/")
+    public String index() {
+        return "login";
+    }
+
+    @Autowired
+    private AdminDOMapper adom;
+
+    /**
+     * 用户登录（临时工能）
+     * @param paramMap
+     * @param username
+     * @param password
+     * @return
+     * @throws ResponseException
+     */
+    @RequestMapping("/login")
+    public String login(Map<String, Object> paramMap,
+                                @RequestParam(name = "username") String username,
+                                @RequestParam(name = "password") String password) throws ResponseException {
+
+        AdminDO adminDO = this.adom.selectByUsernamePassword(username, password);
+        if(adminDO == null) {
+            return "loginfailed";
+        }
+        httpServletRequest.getSession().setAttribute("IS_LOGIN", true);
+        return "loginsuccess";
     }
 }
--- src/main/resources/static/js/backend-page-scripts1.0.1.js
+++ src/main/resources/static/js/backend-page-scripts1.0.1.js
@@ -1,9 +1,24 @@
 // 跳转详情页
 function goDetail(userId, nickName) {
-    window.location.href="/detail?userId=" + userId + "&nickName=" + nickName
+    window.location.href="/detail?userId=" + userId + "&nickName=" + nickName;
 };
 
 // 显示和隐藏文章内容
 function showOrDisplay(clockInId) {
     $("#content" + clockInId).toggle();
+};
+
+// 用户登录
+function login() {
+    var username = $("#username").val();
+    if(username == null || username == ""){
+        alert("用户名不能为空");
+        return false;
+    }
+    var password = $("#password").val();
+    if(password == null || password == ""){
+        alert("密码不能为空");
+        return false;
+    }
+    window.location.href="/login?username=" + username + "&password=" + password;
 }
